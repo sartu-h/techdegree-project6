cac65e9ca2fb8200a033a1cd2953e608efbd5cb1
sartu-h <sartuhussein246@gmail.com>
2026-09-23 00:52:19 -0500

    fixed up checkLetter function

--- js/app.js
+++ js/app.js
@@ -35,16 +35,28 @@ function checkLetter(button){
     let characters = document.getElementsByTagName('li');
     let match = null;
 
-    for(char in characters){
-        if(char.textContent === button.textContent){
+    for(let char of characters){
+        console.log(char);
+        if(char.textContent.toLowerCase() === button.textContent.toLowerCase()){
             char.className = 'show';
-            match.textContent = button.textContent;
+            matcha = button.textContent;
         }
     }
 
     return match;
 }
 
-let word = getRandomPhraseAsArray(phrases);
 
+qwerty.addEventListener('click', (e) => {
+    let button = e.target;
+    if(button.tagName === 'BUTTON' && button.className !== 'chosen'){
+        button.className = 'chosen';
+        let check = checkLetter(button);
+        if(check === null){
+            missed++;
+        }
+    }
+});
 
+let aWord = getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(aWord);
